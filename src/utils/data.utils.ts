d75cc2c4564b62b3a8775137395ccc802b9690ef
Caio Mizumoto <caio.mizumoto@gmail.com>
2022-12-22 09:58:29 -0300

    Added more comments to data.utils file

--- src/utils/data.utils.ts
+++ src/utils/data.utils.ts
@@ -1,6 +1,6 @@
 // Instead putting another arrow to show TS what gets returned from this function, we can just put another ":"
 // Since we don't know what we are getting back, we can use a Generic
-export const getData = async (url: string): Promise<> => {
+export const getData = async <T>(url: string): Promise<T> => {
   const response = fetch(url);
   return (await response).json();
 };
